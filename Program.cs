7f3c4c4ec6c54ecc03f0f43362647d04b6ba1933
Andu <114863878+0xMofarh@users.noreply.github.com>
2025-07-12 04:33:48 +0300

    Update Program.cs
    comment

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 
-namespace ProgrammToic
+namespace ProgrammToicx
 {
     internal class Program
     {
@@ -13,7 +13,7 @@ namespace ProgrammToic
             
             while (true)
             {
-                //Console.Clear();
+                Console.Clear();
                 Console.WriteLine("======= Welcome To AcOn =======");
                 Console.WriteLine("[1] Register Page");
                 Console.WriteLine("[2] Login Page");
@@ -26,76 +26,76 @@ namespace ProgrammToic
                 switch (F)
                 {
                     case 1: 
-                        //Console.Clear();
+                        Console.Clear();
                         Console.WriteLine("======= Register Page =======");
-                        //Console.Write("[!] Enter UserName: ");
-                        //string UserN = Console.ReadLine();
-                        //Console.Write("[!] Enter Password: ");
-                        //string PassN = Console.ReadLine();
-                        //for (int j = 0; j < UserName.Length; j++)
-                        //{
-                        //    tempUser[j] = UserName[j];
-                        //    tempPassword[j] = passwords[j];
-                        //}
-                        //tempUser[tempUser.Length - 1] = UserN;
-                        //UserName = tempUser;
+                        Console.Write("[!] Enter UserName: ");
+                        string UserN = Console.ReadLine();
+                        Console.Write("[!] Enter Password: ");
+                        string PassN = Console.ReadLine();
+                        for (int j = 0; j < UserName.Length; j++)
+                        {
+                            tempUser[j] = UserName[j];
+                            tempPassword[j] = passwords[j];
+                        }
+                        tempUser[tempUser.Length - 1] = UserN;
+                        UserName = tempUser;
 
-                        //tempPassword[tempPassword.Length - 1] = PassN;
-                        //passwords = tempPassword;
+                        tempPassword[tempPassword.Length - 1] = PassN;
+                        passwords = tempPassword;
 
-                        //Console.WriteLine("[!] User Registered: " + UserN);
+                        Console.WriteLine("[!] User Registered: " + UserN);
                         break;
 
                     case 2:
                         Console.Clear();
                         Console.WriteLine("======= Login Page =======");
 
-                        //Console.Write("[!] Enter UserName: ");
-                        //string UserInput = Console.ReadLine();
-                        //Console.Write("[!] Enter Password: ");
-                        //string PasswordInput = Console.ReadLine();
+                        Console.Write("[!] Enter UserName: ");
+                        string UserInput = Console.ReadLine();
+                        Console.Write("[!] Enter Password: ");
+                        string PasswordInput = Console.ReadLine();
 
-                        //bool found = false;
-                        //for (int i = 0; i < UserName.Length; i++)
-                        //{
-                        //    if (UserInput == UserName[i] && PasswordInput == passwords[i])
-                        //    {
-                        //        found = true;
-                        //        break;
-                        //    }
-                        //}
+                        bool found = false;
+                        for (int i = 0; i < UserName.Length; i++)
+                        {
+                            if (UserInput == UserName[i] && PasswordInput == passwords[i])
+                            {
+                                found = true;
+                                break;
+                            }
+                        }
 
-                        //if (found)
-                        //{
-                        //    Console.WriteLine("[!] Welcome sir " + UserInput);
-                        //}
-                        //else
-                        //{
-                        //    Console.WriteLine("[X] Invalid username or password");
-                        //}
+                        if (found)
+                        {
+                            Console.WriteLine("[!] Welcome sir " + UserInput);
+                        }
+                        else
+                        {
+                            Console.WriteLine("[X] Invalid username or password");
+                        }
 
-                        //Console.ReadLine();
-                        //Console.Clear();
+                        Console.ReadLine();
+                        Console.Clear();
                         break;
 
                     case 3:
                         Console.Clear();
                         Console.WriteLine("======= Forgot Page =======");
 
-                        //Console.Write("[!] Enter Your Username : ");
-                        //string forgotUser = Console.ReadLine();
+                        Console.Write("[!] Enter Your Username : ");
+                        string forgotUser = Console.ReadLine();
 
-                        //for (int s = 0; s < UserName.Length; s++) { 
-                        //    if (forgotUser == UserName[s])
-                        //    {
-                        //        Console.WriteLine("[!] Found User ");
-                        //        Console.WriteLine("[!] Enter New Password");
-                        //        string passwd = Console.ReadLine();
-                        //        passwords[s] = passwd;
-                        //        break;
-                        //    }
+                        for (int s = 0; s < UserName.Length; s++) { 
+                            if (forgotUser == UserName[s])
+                            {
+                                Console.WriteLine("[!] Found User ");
+                                Console.WriteLine("[!] Enter New Password");
+                                string passwd = Console.ReadLine();
+                                passwords[s] = passwd;
+                                break;
+                            }
 
-                        //};
+                        };
                         break;
 
                     case 4:
